9a5eea2d525696451059eb0bafa5a37c9061eac4
Dmytro Pishchukhin <dmytro.pishchukhin@gmail.com>
2012-12-24 11:08:24 +0100

    add empty EventAdminUtils and ConfigurationAdminUtils

---
+++ org.knowhowlab.osgi.testing.utils/src/main/java/org/knowhowlab/osgi/testing/utils/cmpn/ConfigurationAdminUtils.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2010-2012 Dmytro Pishchukhin (http://knowhowlab.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.knowhowlab.osgi.testing.utils.cmpn;
+
+/**
+ * OSGi ConfigurationAdmin utilities class
+ *
+ * @author dpishchukhin
+ * @version 1.1
+ * @see org.osgi.framework.Bundle
+ * @see org.osgi.service.cm.ConfigurationAdmin
+ * @see org.osgi.service.cm.Configuration
+ */
+public class ConfigurationAdminUtils {
+    /**
+     * Utility class. Only static methods are available.
+     */
+    private ConfigurationAdminUtils() {
+    }
+}
---
+++ org.knowhowlab.osgi.testing.utils/src/main/java/org/knowhowlab/osgi/testing/utils/cmpn/EventAdminUtils.java
@@ -0,0 +1,20 @@
+package org.knowhowlab.osgi.testing.utils.cmpn;
+
+/**
+ * OSGi EventAdmin utilities class
+ *
+ * @author dpishchukhin
+ * @version 1.0
+ * @see org.osgi.framework.Bundle
+ * @see org.osgi.service.event.EventAdmin
+ * @see org.osgi.service.event.Event
+ * @see org.osgi.service.event.EventConstants
+ * @see org.osgi.service.event.EventHandler
+ */
+public class EventAdminUtils {
+    /**
+     * Utility class. Only static methods are available.
+     */
+    private EventAdminUtils() {
+    }
+}
