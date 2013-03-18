6e7e079566acdc3c9fb1e4d908f5a236b27d42d5
DmytroPishchukhin <dmytro.pishchukhin@gmail.com>
2013-03-18 15:03:10 +0100

    add EventAdmin integration tests

---
+++ org.knowhowlab.osgi.testing.it/org.knowhowlab.osgi.testing.it.pax.exam3/src/test/java/org/knowhowlab/osgi/testing/it/paxexam/NewIn_1_2_EA_IntegrationTest.java
@@ -0,0 +1,69 @@
+package org.knowhowlab.osgi.testing.it.paxexam;
+
+import org.junit.Test;
+import org.knowhowlab.osgi.testing.utils.FilterUtils;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.osgi.framework.InvalidSyntaxException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.knowhowlab.osgi.testing.assertions.OSGiAssert.getBundleContext;
+import static org.knowhowlab.osgi.testing.assertions.cmpn.EventAdminAssert.assertEvent;
+import static org.knowhowlab.osgi.testing.utils.BundleUtils.startBundleAsync;
+import static org.knowhowlab.osgi.testing.utils.cmpn.EventAdminUtils.postEvent;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+
+/**
+ * EventAdmin tests
+ *
+ * @author dpishchukhin
+ */
+public class NewIn_1_2_EA_IntegrationTest extends AbstractTest {
+    private static final String KNOWHOWLAB_TOPICS_TEST = "knowhowlab/topics/test";
+
+    /**
+     * Install test bundle and EventAdmin
+     *
+     * @return config
+     */
+    @Configuration
+    public static Option[] customTestConfiguration() {
+        return combine(baseConfiguration(),
+                mavenBundle("org.apache.felix", "org.apache.felix.eventadmin", "1.3.2"),
+                mavenBundle("org.knowhowlab.osgi", "org.knowhowlab.osgi.testing.it.test.bundle", System.getProperty("project.version")).noUpdate().noStart());
+    }
+
+    @Test
+    public void test_Post_Event() {
+        postEvent(getBundleContext(), KNOWHOWLAB_TOPICS_TEST, 200);
+
+        assertEvent(KNOWHOWLAB_TOPICS_TEST, 500, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void test_Post_Event_With_Filters() throws InvalidSyntaxException {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("prop_key", "val123");
+
+        postEvent(getBundleContext(), KNOWHOWLAB_TOPICS_TEST, props, 200);
+        assertEvent(KNOWHOWLAB_TOPICS_TEST, FilterUtils.eq("prop_key", "val123"), 500, TimeUnit.MILLISECONDS);
+
+        postEvent(getBundleContext(), KNOWHOWLAB_TOPICS_TEST, props, 200);
+        try {
+            assertEvent(KNOWHOWLAB_TOPICS_TEST, FilterUtils.eq("prop_key", "val555"), 500, TimeUnit.MILLISECONDS);
+        } catch (AssertionError e) {
+        }
+    }
+
+    @Test
+    public void test_Service_Event() throws InvalidSyntaxException {
+        // start bundle in 2 sec
+        startBundleAsync(getBundleContext(), "org.knowhowlab.osgi.testing.it.test.bundle", 200);
+
+        assertEvent("org/osgi/framework/ServiceEvent/REGISTERED", FilterUtils.eq("service.objectClass", "org.knowhowlab.osgi.testing.it.testbundle.service.Echo"), 1, TimeUnit.SECONDS);
+    }
+}
