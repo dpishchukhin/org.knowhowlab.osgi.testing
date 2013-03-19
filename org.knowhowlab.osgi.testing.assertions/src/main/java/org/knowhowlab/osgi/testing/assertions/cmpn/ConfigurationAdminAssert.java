a47e7cc8a2e811fc5835be4b087de9aa6f56d405
DmytroPishchukhin <dmytro.pishchukhin@gmail.com>
2013-03-19 16:12:25 +0100

    ConfigurationAdmin assertions - add methods

--- org.knowhowlab.osgi.testing.assertions/src/main/java/org/knowhowlab/osgi/testing/assertions/cmpn/ConfigurationAdminAssert.java
+++ org.knowhowlab.osgi.testing.assertions/src/main/java/org/knowhowlab/osgi/testing/assertions/cmpn/ConfigurationAdminAssert.java
@@ -17,6 +17,15 @@
 package org.knowhowlab.osgi.testing.assertions.cmpn;
 
 import org.knowhowlab.osgi.testing.assertions.OSGiAssert;
+import org.osgi.service.cm.ConfigurationEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static junit.framework.Assert.assertNotNull;
+import static org.knowhowlab.osgi.testing.utils.cmpn.ConfigurationAdminUtils.waitForConfigurationEvent;
+import static org.osgi.service.cm.ConfigurationEvent.CM_DELETED;
+import static org.osgi.service.cm.ConfigurationEvent.CM_UPDATED;
 
 /**
  * A set of OSGi ConfigurationAdmin specific assertion methods useful for writing tests.
@@ -35,4 +44,70 @@ public class ConfigurationAdminAssert extends OSGiAssert {
      */
     private ConfigurationAdminAssert() {
     }
+
+    public static void assertConfigurationEvent(int eventTypeMask, long timeoutInMillis) {
+        assertConfigurationEvent(null, eventTypeMask, timeoutInMillis);
+    }
+
+    public static void assertConfigurationEvent(int eventTypeMask, String pid, String factoryPid, String location, long timeoutInMillis) {
+        assertConfigurationEvent(null, eventTypeMask, pid, factoryPid, location, timeoutInMillis);
+    }
+
+    public static void assertConfigurationUpdated(String pid, String factoryPid, String location, long timeoutInMillis) {
+        assertConfigurationUpdated(null, pid, factoryPid, location, timeoutInMillis);
+    }
+
+    public static void assertConfigurationDeleted(String pid, String factoryPid, String location, long timeoutInMillis) {
+        assertConfigurationDeleted(null, pid, factoryPid, location, timeoutInMillis);
+    }
+
+    public static void assertConfigurationEvent(String message, int eventTypeMask, long timeoutInMillis) {
+        assertConfigurationEvent(message, eventTypeMask, timeoutInMillis, MILLISECONDS);
+    }
+
+    public static void assertConfigurationEvent(String message, int eventTypeMask, String pid, String factoryPid, String location, long timeoutInMillis) {
+        assertConfigurationEvent(message, eventTypeMask, pid, factoryPid, location, timeoutInMillis, MILLISECONDS);
+    }
+
+    public static void assertConfigurationUpdated(String message, String pid, String factoryPid, String location, long timeoutInMillis) {
+        assertConfigurationUpdated(message, pid, factoryPid, location, timeoutInMillis, MILLISECONDS);
+    }
+
+    public static void assertConfigurationDeleted(String message, String pid, String factoryPid, String location, long timeoutInMillis) {
+        assertConfigurationDeleted(message, pid, factoryPid, location, timeoutInMillis, MILLISECONDS);
+    }
+
+    public static void assertConfigurationEvent(int eventTypeMask, long timeout, TimeUnit timeUnit) {
+        assertConfigurationEvent(null, eventTypeMask, null, null, null, timeout, timeUnit);
+    }
+
+    public static void assertConfigurationEvent(int eventTypeMask, String pid, String factoryPid, String location, long timeout, TimeUnit timeUnit) {
+        assertConfigurationEvent(null, eventTypeMask, pid, factoryPid, location, timeout, timeUnit);
+    }
+
+    public static void assertConfigurationUpdated(String pid, String factoryPid, String location, long timeout, TimeUnit timeUnit) {
+        assertConfigurationUpdated(null, pid, factoryPid, location, timeout, timeUnit);
+    }
+
+    public static void assertConfigurationDeleted(String pid, String factoryPid, String location, long timeout, TimeUnit timeUnit) {
+        assertConfigurationDeleted(null, pid, factoryPid, location, timeout, timeUnit);
+    }
+
+    public static void assertConfigurationEvent(String message, int eventTypeMask, long timeout, TimeUnit timeUnit) {
+        assertConfigurationEvent(message, eventTypeMask, null, null, null, timeout, timeUnit);
+    }
+
+    public static void assertConfigurationEvent(String message, int eventTypeMask, String pid, String factoryPid, String location, long timeout, TimeUnit timeUnit) {
+        ConfigurationEvent event = waitForConfigurationEvent(getBundleContext(), eventTypeMask, pid, factoryPid, location, timeout, timeUnit);
+        assertNotNull(message, event);
+    }
+
+    public static void assertConfigurationUpdated(String message, String pid, String factoryPid, String location, long timeout, TimeUnit timeUnit) {
+        assertConfigurationEvent(message, CM_UPDATED, pid, factoryPid, location, timeout, timeUnit);
+    }
+
+    public static void assertConfigurationDeleted(String message, String pid, String factoryPid, String location, long timeout, TimeUnit timeUnit) {
+        assertConfigurationEvent(message, CM_DELETED, pid, factoryPid, location, timeout, timeUnit);
+    }
+
 }
