e4b25b59c2200a2a4a150498383afe8a463c9779
Dmytro Pishchukhin <dmytro.pishchukhin@gmail.com>
2013-03-21 15:55:43 +0100

    starting 1.3.0 ...

---
+++ org.knowhowlab.osgi.testing.utils/src/main/java/org/knowhowlab/osgi/testing/utils/cmpn/WireAdminUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010-2013 Dmytro Pishchukhin (http://knowhowlab.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.knowhowlab.osgi.testing.utils.cmpn;
+
+/**
+ * OSGi WireAdmin utilities class
+ *
+ * @author dpishchukhin
+ * @version 1.0
+ * @see org.osgi.service.wireadmin.Wire
+ * @see org.osgi.service.wireadmin.WireAdmin
+ * @see org.osgi.service.wireadmin.WireAdminEvent
+ * @see org.osgi.service.wireadmin.WireAdminListener
+ * @see org.osgi.service.wireadmin.WireConstants
+ */
+public class WireAdminUtils {
+    /**
+     * Utility class. Only static methods are available.
+     */
+    private WireAdminUtils() {
+    }
+}
