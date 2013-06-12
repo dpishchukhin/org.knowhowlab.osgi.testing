34346f9a448a856fbb3c917cd46df63e899d6049
Dmytro Pishchukhin <dmytro.pishchukhin@gmail.com>
2013-06-12 11:27:20 +0200

    - use PaxExam 3.0.3 - add MonitorAdmin empty Utils and Assertions

---
+++ org.knowhowlab.osgi.testing.assertions/src/main/java/org/knowhowlab/osgi/testing/assertions/cmpn/MonitorAdminAssert.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2010-2013 Dmytro Pishchukhin (http://knowhowlab.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.knowhowlab.osgi.testing.assertions.cmpn;
+
+/**
+ * A set of OSGi MonitorAdmin specific assertion methods useful for writing tests.
+ * <p/>
+ * Before use it could be initialized with default BundleContext
+ * {@link org.knowhowlab.osgi.testing.assertions.OSGiAssert#setDefaultBundleContext(org.osgi.framework.BundleContext)}
+ *
+ * @author dmytro.pishchukhin
+ * @see AssertionError
+ * @see org.knowhowlab.osgi.testing.assertions.OSGiAssert
+ */
+public class MonitorAdminAssert {
+    /**
+     * Utility class. Only static methods are available.
+     */
+    private MonitorAdminAssert() {
+    }
+}
